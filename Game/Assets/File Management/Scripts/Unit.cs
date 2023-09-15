9058b8d330b31fadc17dd38c2443f750808fac98
leejm1902 <leejm1902@gmail.com>
2023-09-15 21:48:18 +0900

    (2023.9.15) CSV 파일 관리

---
+++ Game/Assets/File Management/Scripts/Unit.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Unit : MonoBehaviour
+{
+    [SerializeField] protected new string name;
+    [SerializeField] protected int health;
+    [SerializeField] protected int attack;
+    protected List<Dictionary<string, object>> data;
+
+    protected abstract void Movement();
+
+    public abstract void Initialize(string filename, int count);
+
+    void Update()
+    {
+        Movement();
+    }
+}
